c8542b24cc0a3ea6891b91bb5f6566cd19df1f1b
KuzyaBaka <youryz58@gmail.com>
2023-06-14 15:34:29 +0300

    add Game card

---
+++ src/components/Card/GameCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+function GameCard({ name, img }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia sx={{ height: 140 }} image={img} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default GameCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { actionGames, actionType } from '../../store/actions/gameAction';
-import Card from '../../components/Card/Card';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actionGames, actionType } from "../../store/actions/gameAction";
+import GameCard from "../../components/Card/GameCard";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 function Home() {
-  const { loading, games } = useSelector((state) => state.games)
+  const { loading, games } = useSelector((state) => state.games);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(actionGames.getGames())
+    dispatch(actionGames.getGames());
   }, []);
 
-  console.log(games)
+  console.log(games);
   return (
-    <div>
-      
-    </div>
-  )
+    <Box sx={{ width: "100%" }}>
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 3, md: 3 }}>
+        {!loading ? (
+          games.map((i) => {
+            return (
+              <Grid
+                item
+                xs={3}
+              >
+                <GameCard name={i.name} img={i.background_image} />
+              </Grid>
+            );
+          })
+        ) : (
+          <p>loading</p>
+        )}
+      </Grid>
+    </Box>
+  );
 }
 
-export default Home
+export default Home;
